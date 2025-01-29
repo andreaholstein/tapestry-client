32547c9b4fc46b6e16adabce4b23cda76df93c81
Your Name <you@example.com>
2025-01-29 12:22:21 -0500

    Build HomePage, CommunityCards, SoloCommCards

---
+++ src/components/CommunityCards/CommunityCards.jsx
@@ -0,0 +1,47 @@
+// -------------- FXNALITY --------------
+// -------------- COMPONENTS --------------
+import SoloCommCard from '../SoloCommCard/SoloCommCard'
+// -------------- STYLES --------------
+import './CommunityCards.scss'
+
+function CommunityCards() {
+    const url = import.meta.env.VITE_API_URL;
+
+    // STATE VARIABLES FOR API CALL
+    const [communities, setCommunities] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        // AXIOS GET
+        const getCommunities = async () => {
+            try {
+                // const response = await axios.get(`${url}/communities`);
+                setCommunities(response.data);
+            } catch (error) {
+                console.error(error);
+                setError(error);
+            }
+        };
+        // LOAD API DATA ONTO SCREEN
+        getCommunities();
+    }, []); // [] = runs once + right away!
+
+
+    return (
+        <section className="community-cards">
+            {communities.map((community) => {
+                return (
+                    <Link
+                        to={`path to community`}
+                        key={community.id}
+                        className="community-card__link"
+                    >
+                        <SoloCommCard community={community} />
+                    </Link>
+                )
+            })}
+        </section >
+    )
+}
+
+export default CommunityCards
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+// -------------- FXNALITY --------------
+// -------------- COMPONENTS --------------
+import CommunityCards from '../../components/CommunityCards/CommunityCards'
+// -------------- STYLES --------------
+import './HomePage.scss'
+
+function HomePage() {
+
+    return (
+        <>
+            <CommunityCards />
+        </>
+    )
+}
+
+export default HomePage
