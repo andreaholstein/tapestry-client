df5373caf7ed59f31402d4f74f5ccfe016f42f33
Your Name <you@example.com>
2025-01-28 20:56:22 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// -------------- FXNALITY --------------
+import { BrowserRouter, Router } from 'react-router'
+import { Routes } from 'react-router'
+// -------------- STYLES --------------
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Router>
+
+        </Router>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
