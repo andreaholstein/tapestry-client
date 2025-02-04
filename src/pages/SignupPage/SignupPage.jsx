4e66aba00a47e230b4fd8e31a5f4c60c6ff0b54b
Khushi Parekh <khushi4541@gmail.com>
2025-02-03 19:00:34 -0500

    added in form jsx and created the basic structure

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,9 +1,14 @@
 import SignupCard from "../../components/SignupCard/SignupCard";
+import SignupForm from "../../components/SignupForm/SignupForm";
+import "./SignupPage.scss"
 
-function SignupPage () {
-    return(
-        <SignupCard/>
-    )
+function SignupPage() {
+  return (
+    <section className="signup-page">
+      <SignupCard />
+      <SignupForm />
+    </section>
+  );
 }
 
 export default SignupPage;
