39e0d5aa68ef7fd967766d6d35aff83898bdb555
Savannah Martin-Collins <savannahleemc@gmail.com>
2025-03-05 18:39:15 -0500

    troubleshooting branches

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage/HomePage";
 import CommunityPage from "./pages/CommunityPage/CommunityPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import PostsForm from "./components/PostsForm/PostsForm";
+import SignupPage from './pages/SignupPage/SignupPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 // -------------- COMPONENTS --------------
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 // -------------- STYLES --------------
 import "./styles/global.scss";
 import './App.scss';
-import SignupPage from './pages/SignupPage/SignupPage';
-import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
@@ -24,7 +23,6 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path="/community/:id" element={<CommunityPage />} />
-          {/* <Route path="/community/:communityId/posts" element={<PostsForm />} /> */}
           <Route path="/profile" element={<ProfilePage />} />
         </Routes>
         <Footer />
