1a7f9196b02732bcf24ba51ac888a0d0ce70ae7d
Andrea H <andrea.gitcommit@gmail.com>
2025-03-05 12:20:56 -0500

    Merge Develop Updates

--- src/components/CommunityCards/CommunityCards.jsx
+++ src/components/CommunityCards/CommunityCards.jsx
@@ -15,25 +15,6 @@ function CommunityCards({ communities }) {
       </svg>
       <h3 className="loading__text">Loading Communities</h3>
     </div>
-    return (
-      <div className="loading">
-        <svg
-          className="loading__wheel"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M16 16L19 19M18 12H22M8 8L5 5M16 8L19 5M8 16L5 19M2 12H6M12 2V6M12 18V22"
-            stroke="#f38181"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </svg>
-        <h3 className="loading__text">Loading User</h3>
-      </div>
-    );
   }
 
   return (
